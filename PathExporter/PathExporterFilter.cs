a0ab7e814f2fcc86d3972b0d2a35a69c67d125fc
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2020-12-24 00:15:08 +0300

    Update PathExporter

--- PathExporter/PathExporterFilter.cs
+++ PathExporter/PathExporterFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Numerics;
 using OpenTabletDriver.Plugin;
@@ -10,14 +11,15 @@ namespace AbstractOTDPlugins.PathExporter
     [PluginName("AbstractOTDPlugins.PathExporter")]
     public class PathExporterFilter : IFilter
     {
-        FilterStage IFilter.FilterStage => FilterStage.PostTranspose;
+        FilterStage IFilter.FilterStage => FilterStage.PreInterpolate;
 
         Vector2 IFilter.Filter(Vector2 point)
         {
-            OutFile.WriteLine($"{point.X.ToString(System.Globalization.CultureInfo.InvariantCulture)},{point.Y.ToString(System.Globalization.CultureInfo.InvariantCulture)}");
+            OutFile.WriteLine($"{point.X.ToString(invc)},{point.Y.ToString(invc)}");
             return point;
         }
 
+        private static CultureInfo invc = CultureInfo.InvariantCulture;
         private StreamWriter OutFile = new StreamWriter("output.csv", false);
 
     }
