04cb617d1aa4ef494508dac8d2a23af6eac8e48c
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2021-01-18 23:29:52 +0300

    Update Smoothing property default value

--- BezierInterpolator/BezierInterp.cs
+++ BezierInterpolator/BezierInterp.cs
@@ -36,14 +36,19 @@ namespace BezierInterpolator
             target = Vector3.Lerp(controlPoint, controlPointNext, 0.5f);
         }
 
-        [SliderProperty("Pre-interpolation smoothing factor", 0.01f, 1.0f, 1.0f), ToolTip
+        [Property("Pre-interpolation smoothing factor"), DefaultPropertyValue(1.0f), ToolTip
         (
-            "Sets the factor of pre-interpolation simple exponential smoothing (Aka EMA weight).\n\n" +
+            "Sets the factor of pre-interpolation simple exponential smoothing (aka EMA weight).\n\n" +
             "Possible values are 0.01 .. 1\n" +
             "Factor of 1 means no smoothing is applied,\n" +
             "smaller values add smoothing."
         )]
-        public float emaWeight { get; set; } = 1;
+        public float SmoothingFactor
+        {
+            get { return emaWeight; }
+            set { emaWeight = System.Math.Clamp(value, 0.0f, 1.0f); }
+        }
+        private float emaWeight;
 
         private SyntheticTabletReport SyntheticReport;
         private Vector2 emaTarget;
