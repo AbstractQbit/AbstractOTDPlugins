4ce4648acf1120c1f91b4650656801d8eb37df61
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2021-12-09 12:02:58 +0300

    Add tilt interpolation and smoothing to BezierInterpolator

--- BezierInterpolator/BezierInterp.cs
+++ BezierInterpolator/BezierInterp.cs
@@ -13,12 +13,47 @@ namespace BezierInterpolator
         {
         }
 
+        public override PipelinePosition Position => PipelinePosition.PostTransform;
+
+        [Property("Pre-interpolation smoothing factor"), DefaultPropertyValue(1.0f), ToolTip
+        (
+            "Sets the factor of pre-interpolation simple exponential smoothing (aka EMA weight).\n\n" +
+            "Possible values are 0.01 .. 1\n" +
+            "Factor of 1 means no smoothing is applied, smaller values add smoothing."
+        )]
+        public float SmoothingFactor
+        {
+            get { return emaWeight; }
+            set { emaWeight = System.Math.Clamp(value, 0.0f, 1.0f); }
+        }
+        private float emaWeight;
+
+        [Property("Tilt smoothing factor"), DefaultPropertyValue(1.0f), ToolTip
+        (
+            "For tablets that report tilt, sets the factor of simple exponential smoothing (aka EMA weight) for tilt values.\n\n" +
+            "Possible values are 0.01 .. 1\n" +
+            "Factor of 1 means no smoothing is applied, smaller values add smoothing."
+        )]
+        public float TiltSmoothingFactor
+        {
+            get { return tiltWeight; }
+            set { tiltWeight = System.Math.Clamp(value, 0.0f, 1.0f); }
+        }
+        private float tiltWeight;
+
         protected override void UpdateState()
         {
+            float alpha = (float)(reportStopwatch.Elapsed.TotalSeconds * Frequency / reportMsAvg);
+
+            if (State is ITiltReport tiltReport)
+            {
+                tiltReport.Tilt = Vector2.Lerp(previousTiltTraget, tiltTraget, alpha);
+                State = tiltReport;
+            }
+
             if (State is ITabletReport report && PenIsInRange())
             {
-                float alpha = (float)(reportStopwatch.Elapsed.TotalSeconds * Frequency / reportMsAvg);
-                var lerp1 = Vector3.Lerp(targetOld, controlPoint, alpha);
+                var lerp1 = Vector3.Lerp(previousTarget, controlPoint, alpha);
                 var lerp2 = Vector3.Lerp(controlPoint, target, alpha);
                 var res = Vector3.Lerp(lerp1, lerp2, alpha);
                 report.Position = new Vector2(res.X, res.Y);
@@ -36,36 +71,29 @@ namespace BezierInterpolator
                 if (consumeDelta < 150)
                     reportMsAvg += ((consumeDelta - reportMsAvg) * 0.1f);
 
+                emaTarget = vec2IsFinite(emaTarget) ? emaTarget : report.Position;
                 emaTarget += emaWeight * (report.Position - emaTarget);
 
                 controlPoint = controlPointNext;
                 controlPointNext = new Vector3(emaTarget, report.Pressure);
 
-                targetOld = target;
+                previousTarget = target;
                 target = Vector3.Lerp(controlPoint, controlPointNext, 0.5f);
+            }
+
+            if (State is ITiltReport tiltReport)
+            {
+                if (!vec2IsFinite(tiltTraget)) tiltTraget = tiltReport.Tilt;
+                previousTiltTraget = tiltTraget;
+                tiltTraget += tiltWeight * (tiltReport.Tilt - tiltTraget);
             }
         }
 
-        [Property("Pre-interpolation smoothing factor"), DefaultPropertyValue(1.0f), ToolTip
-        (
-            "Sets the factor of pre-interpolation simple exponential smoothing (aka EMA weight).\n\n" +
-            "Possible values are 0.01 .. 1\n" +
-            "Factor of 1 means no smoothing is applied,\n" +
-            "smaller values add smoothing."
-        )]
-        public float SmoothingFactor
-        {
-            get { return emaWeight; }
-            set { emaWeight = System.Math.Clamp(value, 0.0f, 1.0f); }
-        }
-
-        public override PipelinePosition Position => PipelinePosition.PostTransform;
-
-        private float emaWeight;
-
-        private Vector2 emaTarget;
-        private Vector3 controlPointNext, controlPoint, target, targetOld;
+        private Vector2 emaTarget, tiltTraget, previousTiltTraget;
+        private Vector3 controlPointNext, controlPoint, target, previousTarget;
         private HPETDeltaStopwatch reportStopwatch = new HPETDeltaStopwatch();
         private float reportMsAvg = 5;
+
+        private bool vec2IsFinite(Vector2 vec) => float.IsFinite(vec.X) & float.IsFinite(vec.Y);
     }
 }
