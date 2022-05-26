2707316c159a677b8a9fef7a288a608fdf694be0
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2022-05-26 11:16:10 +0300

    Add sin(cursor) plugin

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Interpolator that spatially smoothes out drawn lines by interpreting pen path as
 ## [Radial Follow Smoothing](RadialFollow/README.md)
 The Ultimate Smoothing Plugin, providing cursor stability while still allowing to hit fast jumps with ease.  
 To view how different parameters affect the operating curve, open [RadialFollow/RadialFollowGraphs.ggb](RadialFollow/RadialFollowGraphs.ggb) in a [GeoGebra calculator](https://www.geogebra.org/calculator).
+
+## Touch Tapping
+Allows using tablets with touch sensitivity as a two key tapping pad.
+
+
+## sin(cursor)
+Makes the cursor wiggle in a sine wave.
---
+++ SinCursor/SinCursor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;
+
+namespace SinCursor
+{
+    [PluginName("sin(cursor)")]
+    public class SinCursor : IPositionedPipelineElement<IDeviceReport>
+    {
+        public SinCursor() : base() { }
+        public PipelinePosition Position => PipelinePosition.PostTransform;
+
+        [Property("Amount"), DefaultPropertyValue(10.0f)]
+        public float Amount
+        {
+            get { return amount; }
+            set { amount = Math.Clamp(value, 0.0f, 1000.0f); }
+        }
+        private float amount;
+
+
+        [Property("Freq"), DefaultPropertyValue(10.0f)]
+        public float Freq
+        {
+            get { return freq; }
+            set { freq = Math.Clamp(value, 0.0f, 1000.0f); }
+        }
+        private float freq;
+
+        [Property("Elastic amplitude"), DefaultPropertyValue(true)]
+        public bool ElasticAmp { get; set; }
+
+        public event Action<IDeviceReport> Emit;
+
+        public void Consume(IDeviceReport value)
+        {
+            if (value is ITabletReport report)
+            {
+                if (!vec2IsFinite(thing)) thing = report.Position;
+
+                var delta = report.Position - thing;
+                thing += Vector2.Normalize(delta) * Math.Max(delta.Length() - amount, 0);
+                if (!ElasticAmp) delta = report.Position - thing;
+
+                report.Position += MathF.Sin((float)stopwatch.Elapsed.TotalSeconds * MathF.PI * 2 * freq)
+                                 * Vector2.Transform(delta, Matrix3x2.CreateRotation(MathF.PI / 2));
+                value = report;
+            }
+            Emit?.Invoke(value);
+        }
+
+        private Vector2 thing;
+        private HPETDeltaStopwatch stopwatch = new HPETDeltaStopwatch();
+        private bool vec2IsFinite(Vector2 vec) => float.IsFinite(vec.X) & float.IsFinite(vec.Y);
+    }
+}
